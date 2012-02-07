49fbf9ce0a6860b17edd32aed3b3c8e90513f03c
manitourobotics <steelstangs@gmail.com>
2012-02-07 11:09:53 -0700

    Update robot.py_stuff/robot_test_harness.py

--- robot.py_stuff/robot_test_harness.py
+++ robot.py_stuff/robot_test_harness.py
@@ -5,5 +5,41 @@ if CANJaguar(1) = True,
      
      else:
           print "CANJaguar #1 is not opporational"
+          
+if CANJaguar(2) = True,
+     print "CanJaguar #2 is opporational"
+     
+     else:
+          print "CANJaguar #2 is not opporational"
+          
+if CANJaguar(3) = True,
+     print "CanJaguar #3 is opporational"
+     
+     else:
+          print "CANJaguar #3 is not opporational"
+          
+if CANJaguar(4) = True,
+     print "CanJaguar #4 is opporational"
+     
+     else:
+          print "CANJaguar #4 is not opporational"
+          
+if CANJaguar(5) = True,
+     print "CanJaguar #5 is opporational"
+     
+     else:
+          print "CANJaguar #5 is not opporational"
+          
+if CANJaguar(6) = True,
+     print "CanJaguar #6 is opporational"
+     
+     else:
+          print "CANJaguar #6 is not opporational"
+          
+if CANJaguar(7) = True,
+     print "CanJaguar #7 is opporational"
+     
+     else:
+          print "CANJaguar #7 is not opporational"
 
 

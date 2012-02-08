7f0d86f27eb7ca6444c92fd7de7acebcc03f857e
manitourobotics <steelstangs@gmail.com>
2012-02-07 18:06:36 -0700

    Update robot.py_stuff/robot_test_harness.py

--- robot.py_stuff/robot_test_harness.py
+++ robot.py_stuff/robot_test_harness.py
@@ -1,60 +1,60 @@
 import robot_in_python.py
 
-if CANJaguar(1) = True,
-     print "CanJaguar #1 is opporational"
+if wpilib.Joystick(1) = True,
+     print "Joystick #1 is opporational"
      
      else:
-          print "CANJaguar #1 is not opporational"
+          print "Joystick #1 is not opporational"
           
-if CANJaguar(2) = True,
-     print "CanJaguar #2 is opporational"
+if wpilib.Joystick(2) = True,
+     print "Joystick #2 is opporational"
      
      else:
-          print "CANJaguar #2 is not opporational"
+          print "Joystick #2 is not opporational"
           
-if CANJaguar(3) = True,
+if wpilib.CANJaguar(3) = True,
      print "CanJaguar #3 is opporational"
      
      else:
           print "CANJaguar #3 is not opporational"
           
-if CANJaguar(4) = True,
+if wpilib.CANJaguar(4) = True,
      print "CanJaguar #4 is opporational"
      
      else:
           print "CANJaguar #4 is not opporational"
           
-if CANJaguar(5) = True,
+if wpilib.CANJaguar(5) = True,
      print "CanJaguar #5 is opporational"
      
      else:
           print "CANJaguar #5 is not opporational"
           
-if CANJaguar(6) = True,
+if wpilib.CANJaguar(6) = True,
      print "CanJaguar #6 is opporational"
      
      else:
           print "CANJaguar #6 is not opporational"
           
-if CANJaguar(7) = True,
+if wpilib.CANJaguar(7) = True,
      print "CanJaguar #7 is opporational"
      
      else:
           print "CANJaguar #7 is not opporational"
           
-if CANJaguar(8) = True,
+if wpilib.CANJaguar(8) = True,
      print "CanJaguar #8 is opporational"
      
      else:
           print "CANJaguar #8 is not opporational"
           
-if CANJaguar(9) = True,
+if wpilib.CANJaguar(9) = True,
      print "CanJaguar #9 is opporational"
      
      else:
           print "CANJaguar #9 is not opporational"
           
-if CANJaguar(10) = True,
+if wpilib.CANJaguar(10) = True,
      print "CanJaguar #10 is opporational"
      
      else:

3e739e9c48f3b7a5f99a2017f60e085badcd853f
manitourobotics <steelstangs@gmail.com>
2012-02-11 12:37:29 -0700

    Update robot.py_stuff/robot_test_harness.py

--- robot.py_stuff/robot_test_harness.py
+++ robot.py_stuff/robot_test_harness.py
@@ -13,23 +13,32 @@ Tank Drive Test                    |
 
 def test(1):
      print 'Begining to test tank drive'
-     time.wait (2)
+     time.wait (2.0)
      """
      test
      """
      print 'Finished testing tank drive.'
            'The tank drive is reliable '%' percent of the time and failed '%' tests of the '%'
-     
+
 """
 Launcher Test                      |
                                    V
 """
 
 def test(2):
-     print 'Begining to the launching system'
-     time.wait (2)
+     print 'Begining to test the launching system'
+     time.wait (2.0)
      """
      test
      """
      print 'Finished testing the launching system.'
            'The launching system is reliable '%' percent of the time and failed '%' tests of the '%'
+
+def test(3):
+     print 'Begining to test the acquisition system'
+     time.wait (2.0)
+     """
+     test
+     """
+     print 'Finished testing the acquisition system.'
+           'The acquisition system is reliable '%' percent of the time and failed '%' tests of the '%'

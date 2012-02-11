489e30d3da6dd6cb112c42653c039766794e8bf1
manitourobotics <steelstangs@gmail.com>
2012-02-11 11:44:44 -0700

    Update robot.py_stuff/robot_test_harness.py

--- robot.py_stuff/robot_test_harness.py
+++ robot.py_stuff/robot_test_harness.py
@@ -12,7 +12,8 @@ def test(1):
      """
      test
      """
-     print 'Finished testing tank drive. The tank drive is reliable '%' percent of the time and failed '%' tests of the '%' 
+     print 'Finished testing tank drive.'
+           'The tank drive is reliable '%' percent of the time and failed '%' tests of the '%'
      
 """
 Launcher Test                      |
@@ -25,77 +26,5 @@ def test(2):
      """
      test
      """
-     print 'Finished testing the launching system. The launching system is reliable '%' percent of the time and failed '%' tests of the '%' 
-          
-
-     
-
-"""
-if wpilib.Joystick(1) = True,
-     print "Joystick #1 is opporational"
-     
-     else:
-          print "Joystick #1 is not opporational"
-          
-if wpilib.Joystick(2) = True,
-     print "Joystick #2 is opporational"
-     
-     else:
-          print "Joystick #2 is not opporational"
-          
-if wpilib.Joystick(1) = False and
-   wpilib.Joystick(2) = False,
-          print "Tank drive is not opporational"
-          
-if wpilib.CANJaguar(3) = True,
-     print "CanJaguar #3 is opporational"
-     
-     else:
-          print "CANJaguar #3 is not opporational"
-          
-if wpilib.CANJaguar(4) = True,
-     print "CanJaguar #4 is opporational"
-     
-     else:
-          print "CANJaguar #4 is not opporational"
-          
-if wpilib.CANJaguar(5) = True,
-     print "CanJaguar #5 is opporational"
-     
-     else:
-          print "CANJaguar #5 is not opporational"
-          
-if wpilib.CANJaguar(6) = True,
-     print "CanJaguar #6 is opporational"
-     
-     else:
-          print "CANJaguar #6 is not opporational"
-          
-if wpilib.CANJaguar(7) = True,
-     print "CanJaguar #7 is opporational"
-     
-     else:
-          print "CANJaguar #7 is not opporational"
-          
-if wpilib.CANJaguar(8) = True,
-     print "CanJaguar #8 is opporational"
-     
-     else:
-          print "CANJaguar #8 is not opporational"
-          
-if wpilib.CANJaguar(9) = True,
-     print "CanJaguar #9 is opporational"
-     
-     else:
-          print "CANJaguar #9 is not opporational"
-          
-if wpilib.CANJaguar(10) = True,
-     print "CanJaguar #10 is opporational"
-     
-     else:
-          print "CANJaguar #10 is not opporational"
-          
-if wpilib.CANJaguar = False,
-     print "Fix your damn code"
-"""
-
+     print 'Finished testing the launching system.'
+           'The launching system is reliable '%' percent of the time and failed '%' tests of the '%'

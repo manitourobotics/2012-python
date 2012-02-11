1f10987e9681ee3ea5eb5a2eff0c682b99053006
ben <steelstangs@gmail.com>
2012-02-11 15:36:01 -0700

    added test harness

---
+++ robot.py_stuff/tests/robot_test_harness.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import unittest
+
+"""
+Goals: Write the testing system for tank drive & launcher (acquisition) by Wendsday.
+"""
+
+
+sys.path.append('../')
+import robot_python_new as mod
+
+def suite():
+
+   suite = unittest.TestSuite()
+   suite.addTest(unittest.makeSuite(TestTank))
+   
+   return suite
+
+
+class TestTank(unittest.TestCase):
+   
+
+     def test_tank(self):
+          self.assertTrue(1==1)
+
+
+if __name__ == "__main__":
+
+   runner = unittest.TextTestRunner(verbosity=2)
+   runner.run(suite())

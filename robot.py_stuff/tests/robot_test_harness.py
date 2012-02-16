e93f43cc8a12074865f39e05d01bb66beea7c392
manitourobotics <steelstangs@gmail.com>
2012-02-15 17:03:54 -0700

    Update robot.py_stuff/tests/robot_test_harness.py

--- robot.py_stuff/tests/robot_test_harness.py
+++ robot.py_stuff/tests/robot_test_harness.py
@@ -5,6 +5,16 @@ import time
 import unittest
 import pygame
 
+screen = pygame.display.set_mode((640, 400))
+running = 1
+
+while running:
+   event = pygame.event.poll()
+   if event.type == pygame.QUIT:
+      running = 0
+   screen.fill((255, 255, 255))
+   pygame.display.flip()
+
 """
 Goals: Write the testing system for tank drive & launcher (acquisition) by Wendsday.
 """
